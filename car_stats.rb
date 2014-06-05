06ba20ae2086e7213201dd5e4ea0c85af412d258
Sachin Ahuja <sachinahj@gmail.com>
2014-06-05 10:54:44 -0500

    made all tests pass

--- car_stats.rb
+++ car_stats.rb
@@ -1,3 +1,14 @@
 class CarStats
   # TODO
+  def self.calc_top_color(cars)
+    freq = cars.inject(Hash.new(0)) { |h,v| h[v.color] += 1; h }
+    p freq
+    car = cars.max_by { |v| freq[v.color] }
+    car.color
+  end
+  def self.calc_bottom_color(cars)
+    freq = cars.inject(Hash.new(0)) { |h,v| h[v.color] += 1; h }
+    car = cars.min_by { |v| freq[v.color] }
+    car.color
+  end
 end
