c8e0201dbc199770e292c345791d834af0eb1073
Sachin Ahuja <sachinahj@gmail.com>
2014-06-05 15:22:38 -0500

    completed assignment, still need to work on extensions

--- bar.rb
+++ bar.rb
@@ -1,4 +1,48 @@
 require 'time' # you're gonna need it
 
+class Item
+  attr_reader :name, :price
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+end
+
+
 class Bar
+  attr_reader :name
+  attr_accessor :menu_items
+
+  def initialize(name)
+    @name = name
+    @menu_items = []
+    @happy_discount = 0
+  end
+  def add_menu_item(name, price)
+    @menu_items << Item.new(name, price)
+  end
+  def happy_discount
+    if happy_hour?
+      return @happy_discount
+    else
+      return 0
+    end    
+  end
+
+  def happy_discount=(discount)
+    if discount > 1
+      @happy_discount = 1
+    elsif discount < 0
+      @happy_discount = 0
+    else
+      @happy_discount = discount
+    end
+  end
+  def happy_hour?
+    if Time.now.hour == 15
+      return true
+    else 
+      return false
+    end
+  end
 end
