0ebb69bee8f55dbbefe2f29a7d5ca4efe09790aa
Sachin Ahuja <sachinahj@gmail.com>
2014-06-25 10:39:09 -0500

    completed code to make tests pass

--- lib/honkr/scripts/sign_in.rb
+++ lib/honkr/scripts/sign_in.rb
@@ -3,7 +3,34 @@ module Honkr
 
     def self.run(params)
       # TODO
+      # p "params --> #{params}"
+      user = Honkr.db.get_user_by_username(params[:username])
+      # p "user --> #{user}"
+      if  user == nil
+        return {
+          :success? => false,
+          :error => :no_user_exist,
+          :session_id => "no id"
+        }
+      elsif user.has_password?(params[:password]) == false 
+        return {
+          :success? => false,
+          :error => :invalid_password,
+          :session_id => "no id"
+        }
+      elsif user.has_password?(params[:password]) == true
+        return {
+          :success? => true,
+          :error => :none,
+          :session_id => Honkr.db.create_session(:user_id => user.id)
+        }
+      else
+        return {
+          :success? => false,
+          :error => :no_idea,
+          :session_id => "no id"
+        }
+      end
     end
-
   end
 end
