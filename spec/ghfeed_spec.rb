63347966cbc0858a37038ec0fc5d923d021806c3
Sachin Ahuja <sachinahj@gmail.com>
2014-07-07 11:09:32 -0500

    initial commit after mac switch

---
+++ spec/ghfeed_spec.rb
@@ -0,0 +1,11 @@
+require 'rubygems'
+require 'rspec'
+require_relative '../lib/github.rb'
+
+describe GHFeed::RepoInfo do
+  describe '.get_repo_data'
+end
+
+describe GHFeed::UserInfo do
+  describe '.get_user_data'
+end
